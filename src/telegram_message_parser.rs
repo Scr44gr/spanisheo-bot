8249210bce198d143ba8885f44b1a7a2ff4a42e7
Abrahan <scr44gr@gmail.com>
2022-02-06 08:32:00 -0400

    r: refactor telegram_message_parser

--- src/telegram_message_parser.rs
+++ src/telegram_message_parser.rs
@@ -1,41 +1,40 @@
-
-pub mod parser{
-    use serde_json::{Value};
+pub mod parser {
+    use serde_json::Value;
 
     pub trait MessageParser {
-        fn get_json_content(&self) -> &Vec<Value>;
+        fn get_json_content(&self) -> Vec<Value>;
         fn get_message(&self) -> String;
-        fn parse_message(&self) -> Vec<String>;
     }
-    
+
     pub struct TelegramParser {
         pub message: Value,
     }
-    
+
     impl MessageParser for TelegramParser {
-        fn get_json_content(&self)-> &Vec<Value>{
-           let content = &self.message[0]["def"][0]["sseq"][0][0][1]["dt"][1][1];
-           if content.is_array(){
-               return content.as_array().unwrap();
-           }else{
-               return &self.message[1]["shortdef"].as_array().unwrap(); 
-           }
+        fn get_json_content(&self) -> Vec<Value> {
+
+            let content = &self.message[0]["shortdef"];
+            let mut response: Vec<Value> = Vec::new();
+
+            if content.is_array() {
+                if !content.as_array().is_none() {
+                    response = content.as_array().unwrap().to_vec();
+                }
+            } else {
+                if !self.message[1]["shortdef"].as_array().is_none() {
+                    response = self.message[1]["shortdef"].as_array().unwrap().to_vec();
+                }
+            }
+            response
         }
         fn get_message(&self) -> String {
-            let result: Vec<String> = self.parse_message();
-            let message = result.join("\n");
+            let result: Vec<Value> = self.get_json_content();
+            let mut message: String = String::from("");
+            for item in result {
+                message.push_str(&item.as_str().unwrap());
+                message.push_str("\n\n");
+            }
             message
         }
-        fn parse_message(&self) -> Vec<String> {
-            let mut result: Vec<String> = vec![];
-            for value in  self.get_json_content(){
-                let spanish_traduction = value.get("tr").unwrap();
-                let english_traduction = value.get("t").unwrap();
-    
-                result.push(String::from(format!("English: {}\nSpanish: {}\n", &english_traduction, &spanish_traduction )));
-            }
-            result
-        }
     }
-    
 }
